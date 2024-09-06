76e6f370d3ad821a3cb7276b18b5496bb8e6096d
basepointweb <basepointweb@gmail.com>
2024-09-05 21:56:10 -0300

    First commit

---
+++ SolutionTemplate/BPTemplate.Core/SHARED/SharedConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace $safeprojectname$.Shared
+{
+    public static class SharedConstants
+    {
+        public static class ErrorMessages
+        {
+            public static readonly string PropertyIsInvalid = "001;The property '{0}' is invalid.";
+        }
+    }
+}
---
+++ SolutionTemplate/BPTemplate.Presentation.AspNetCoreApi/Program.cs
@@ -0,0 +1,26 @@
+using $ext_safeprojectname$.Cqrs.Dapper.Configurations;
+using $ext_safeprojectname$.Cqrs.Dapper.Migrations;
+using $ext_safeprojectname$.Core.Configurations;
+using $ext_safeprojectname$.Presentation.AspNetCoreApi.Configurations;
+using BasePoint.Core.Presentation.AspNetCoreApi.Configurations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var section = builder.Configuration.GetSection("AppSettings");
+
+builder.Services.MapRepositories();
+builder.Services.MapValidations();
+builder.Services.MapUseCases();
+builder.Services.MapCommandProviders();
+builder.Services.MapQueryProviders();
+builder.Services.MapHttpContextAccessor();
+builder.Services.MapUnitOfWork();
+builder.Services.MapConnection(section);
+
+var app = DefaultApiConfiguration.Configure(builder);
+
+Migrations.CreateDataBase(section["DatabaseServerName"], section["DatabaseName"]);
+
+Migrations.Migrate(section["DatabaseServerName"], section["DatabaseName"], section["DatabaseUser"], section["DatabasePassWord"]);
+
+app.Run();
